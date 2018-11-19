cda0f75c82cdf804495ddf93c49c40b7dec9187a
justinjoh <4justin.oh@gmail.com>
2018-11-18 22:56:15 -0500

    plotting works, function may not be correct

--- aep4500fa18/hw04_6.py
+++ aep4500fa18/hw04_6.py
@@ -1,17 +1,23 @@
-import numpy
+import numpy as np
 import math
 import matplotlib.pyplot as plt
 
-def psi_k(x, G_range):
-    k = 2*math.pi/10
-    len_G_range = len(G_range)
+
+def psi_k(x, g_range):
+    print("A")
+    k = 6*math.pi/10
+    len_g_range = len(g_range)
     return_val = 0
-    #for m in range(len_G_range):
-    #    return_val += (.02/m)*
-    return None
+    for m in range(len_g_range):
+        g_val = g_range[m]
+        return_val += (.02/(m+1)*np.exp(-1j*(k-g_val)*x))
+    return return_val
 
 
 if __name__ == "__main__":
-
-    # psi_k()
-    pass
+    print("working")
+    x_values = np.arange(0, 10, .01)
+    G_range = np.arange(-8*np.pi, +8*np.pi, 2*np.pi)
+    plt.figure(1)
+    plt.plot(x_values, np.real(psi_k(x_values, G_range)))
+    plt.show()
