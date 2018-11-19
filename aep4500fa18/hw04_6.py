eb4114c96d22f9da461c4e116ba392f4da95d154
justinjoh <4justin.oh@gmail.com>
2018-11-19 00:39:53 -0500

    plots psi, psi^2, and u over range of c, x, and G

--- aep4500fa18/hw04_6.py
+++ aep4500fa18/hw04_6.py
@@ -3,21 +3,47 @@ import math
 import matplotlib.pyplot as plt
 
 
-def psi_k(x, g_range):
-    print("A")
-    k = 6*math.pi/10
+def psi_k(c, k, x, g_range):
     len_g_range = len(g_range)
     return_val = 0
     for m in range(len_g_range):
-        g_val = g_range[m]
-        return_val += (.02/(m+1)*np.exp(-1j*(k-g_val)*x))
+        if m != 0:
+            return_val += (c/(m+1)*np.exp(1j*(k-g_range[m])*x))
+            return_val += (c/(m+1)*np.exp(1j*(k+g_range[m])*x))
+        else:
+            return_val += (c/(m+1)*np.exp(1j*(k-g_range[m])*x))
+
+    return return_val
+
+
+def u_k(c, x, g_range):
+    len_g_range = len(g_range)
+    return_val = 0
+    for m in range(len_g_range):
+        return_val += (c/(m+1)*np.exp(1j*(g_range[m])*x))
+        return_val += (c/(m+1)*np.exp(1j*(g_range[m])*x))
+    else:
+        return_val += (c/(m+1)*np.exp(1j*(g_range[m])))
+
     return return_val
 
 
 if __name__ == "__main__":
-    print("working")
+    k_val = 4 * math.pi / 10
+    c_values = [.05, .2, .4, 1, 10, 100]
     x_values = np.arange(0, 10, .01)
-    G_range = np.arange(-8*np.pi, +8*np.pi, 2*np.pi)
-    plt.figure(1)
-    plt.plot(x_values, np.real(psi_k(x_values, G_range)))
+    G_range = np.arange(0, +8*np.pi, 2*np.pi)
+
+    num_cvals = len(c_values)
+    fig, axs = plt.subplots(nrows=num_cvals, ncols=3)
+    for i in range(num_cvals):
+        ax = axs[i, 0]
+        ax.plot(x_values, np.real(psi_k(c_values[i], k_val, x_values, G_range)))
+
+        ax = axs[i, 1]
+        ax.plot(x_values, np.conj(psi_k(c_values[i], k_val, x_values, G_range))*psi_k(c_values[i], k_val, x_values, G_range))
+
+        ax = axs[i, 2]
+        ax.plot(x_values, np.real(u_k(c_values[i], x_values, G_range)))
+
     plt.show()
