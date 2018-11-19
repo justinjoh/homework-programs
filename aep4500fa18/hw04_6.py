13d4dcb5d234d40337523ff68473e9ecd8c1eab5
justinjoh <4justin.oh@gmail.com>
2018-11-18 22:19:35 -0500

    start writing, not functional

---
+++ aep4500fa18/hw04_6.py
@@ -0,0 +1,17 @@
+import numpy
+import math
+import matplotlib.pyplot as plt
+
+def psi_k(x, G_range):
+    k = 2*math.pi/10
+    len_G_range = len(G_range)
+    return_val = 0
+    #for m in range(len_G_range):
+    #    return_val += (.02/m)*
+    return None
+
+
+if __name__ == "__main__":
+
+    # psi_k()
+    pass
